41e85ae55db6e33f6378298910337996aa8adc12
honnkon <146614313+honnkon@users.noreply.github.com>
2024-01-03 18:18:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(function() {
                 } else if(json.message == "There is a same data"){
                     $("#save_response").text("同じ閲覧コードがあります。別のコードを入力してください。");
                 } else {
-                    $("#save_response").html(`成功しました。↓共有用<br/><textarea readonly>閲覧コード：${$("#save_showcode").val()}\nこれは、パスワードマネージャー（https://honnkon.github.io/save-code-manager/）で利用できるコードです。</textarea>`);
+                    $("#save_response").html(`成功しました。↓共有用<br/><textarea readonly>閲覧コード：${$("#save_showcode").val()}\nこれは、パスワードマネージャー（https://sites.google.com/view/honnkon/save-code-manager）で利用できるコードです。</textarea>`);
                 }
             });
         })
